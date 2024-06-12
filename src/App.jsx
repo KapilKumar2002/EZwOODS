5787d22a46ad5ed8a0cfe6f39c366e8f1fabd640
KapilKumar2002 <kapil012002@gmail.com>
2024-06-12 17:29:01 +0530

    folder structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./layout/Layout";
+import HomePage from "./pages/home/HomePage";
+
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [{ path: "/", element: <HomePage /> }],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
