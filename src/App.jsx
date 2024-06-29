3ce7aba055a03050dd96a76554c9bd458fbf6680
KapilKumar2002 <kapil012002@gmail.com>
2024-06-29 21:33:08 +0530

    comit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Layout from "./layout/Layout";
 import HomePage from "./pages/home/HomePage";
 import CartPage from "./pages/cart/CartPage";
 import ProductPage from "./pages/product/ProductPage";
-import ProductDetail from "./pages/detail/ProductDetail";
 import WishListPage from "./pages/wishlist/WishListPage";
 import AboutPage from "./pages/about/AboutPage";
+import ProductView from "./pages/detail/productCarousel/ProductView";
 
 function App() {
   const router = createBrowserRouter([
@@ -17,7 +17,7 @@ function App() {
         { path: "/product", element: <ProductPage /> },
         { path: "/cart", element: <CartPage /> },
         { path: "/wishlist", element: <WishListPage /> },
-        { path: "/product/1", element: <ProductDetail /> },
+        { path: "/product/1", element: <ProductView /> },
         { path: "/about", element: <AboutPage /> },
       ],
     },
