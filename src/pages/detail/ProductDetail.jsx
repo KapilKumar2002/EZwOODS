ad75819a193bd4bfc364e0d70868845386a7f474
KapilKumar2002 <kapil012002@gmail.com>
2024-06-29 21:40:47 +0530

    some bugs are fixed

---
+++ src/pages/detail/ProductDetail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductView from "./productCarousel/ProductView";
+
+const ProductDetail = () => {
+  return (
+    <div className="container mx-auto">
+      <ProductView />
+    </div>
+  );
+};
+
+export default ProductDetail;
--- src/pages/detail/productCarousel/ProductView.jsx
+++ src/pages/detail/productCarousel/ProductView.jsx
@@ -1,54 +1,70 @@
 import React, { useState } from "react";
+import Slider from "react-slick";
+import carousel1 from "../../../assets/carousel1.png";
 import RatingReadOnly from "../../../components/atoms/RatingReadOnly";
 import ProductTabs from "../../../components/atoms/ProductTabs";
 
-
 const ProductView = () => {
-  const [images, setImages] = useState({
-    img1: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,b_rgb:f5f5f5/3396ee3c-08cc-4ada-baa9-655af12e3120/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    img2: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/e44d151a-e27a-4f7b-8650-68bc2e8cd37e/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    img3: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/44fc74b6-0553-4eef-a0cc-db4f815c9450/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    img4: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/d3eb254d-0901-4158-956a-4610180545e5/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-  });
-
-  const [activeImg, setActiveImage] = useState(images.img1);
-
   const [amount, setAmount] = useState(1);
 
-  return (
-    <div className="flex flex-col lg:flex-row gap-16 ">
-      <div className="flex flex-col gap-6 lg:w-2/4">
+  const slides = [
+    { image: carousel1 },
+    { image: carousel1 },
+    { image: carousel1 },
+  ];
+  // const [activeImg, setActiveImage] = useState(images.img1);
+
+  const [dotActive, setDocActive] = useState(0);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    pauseOnHover: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    autoplay: true,
+    beforeChange: (prev, next) => {
+      setDocActive(next);
+    },
+    appendDots: (dots) => (
+      <div
+        style={{
+          height: "120px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div className="flex md:gap-20 gap-14">{dots}</div>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div className="md:w-24 w-20 absolute -translate-x-[50%] left-[50%] flex justify-center items-center">
         <img
-          src={activeImg}
+          src={slides[i].image}
           alt=""
-          className="w-full h-full aspect-square object-cover rounded-xl"
+          className={`md:h-24 h-20 md:w-24 w-20 object-cover rounded-md ${
+            i == dotActive ? "border-2 border-white" : ""
+          }`}
         />
-        <div className="flex flex-row justify-between h-24">
-          <img
-            src={images.img1}
-            alt=""
-            className="w-24 h-24 rounded-md cursor-pointer"
-            onClick={() => setActiveImage(images.img1)}
-          />
-          <img
-            src={images.img2}
-            alt=""
-            className="w-24 h-24 rounded-md cursor-pointer"
-            onClick={() => setActiveImage(images.img2)}
-          />
-          <img
-            src={images.img3}
-            alt=""
-            className="w-24 h-24 rounded-md cursor-pointer"
-            onClick={() => setActiveImage(images.img3)}
-          />
-          <img
-            src={images.img4}
-            alt=""
-            className="w-24 h-24 rounded-md cursor-pointer"
-            onClick={() => setActiveImage(images.img4)}
-          />
-        </div>
+      </div>
+    ),
+  };
+
+  return (
+    <div className="flex flex-col justify-between lg:flex-row gap-16 lg:items-center">
+      <div className="flex flex-col gap-6 lg:w-2/4">
+        <Slider className="w-full" {...settings}>
+          {slides.map((slide, index) => (
+            <div className="md:h-[550px] h-96 w-full outline-none duration-500">
+              <img
+                src={slide.image}
+                alt=""
+                className="md:h-[450px] h-72 w-full object-cover rounded-xl  aspect-square duration-700"
+              />
+            </div>
+          ))}
+        </Slider>
       </div>
       {/* ABOUT */}
       <div className="flex flex-col gap-3 w-[50%]">
@@ -105,42 +121,44 @@ const ProductView = () => {
             <div class="w-6 h-6 bg-green-500 rounded-full"></div>
           </div>
         </div>
-          <div class="bg-white shadow-lg rounded-md p-4 w-full max-w-lg">
-            <div class="flex justify-between items-center mb-4 gap-6">
-            <div><span className="text-sm">Price</span>
-            <div class="text-2xl font-semibold">$99.99</div></div>
-              <button class="bg-black text-white px-4 py-3 text-sm rounded-3xl w-full flex items-center justify-center">
-                Shop Now
-                <svg
-                  class="w-4 h-4 ml-2"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 12h14m0 0l-6-6m6 6l-6 6"
-                  ></path>
-                </svg>
-              </button>
+        <div class="bg-white shadow-lg rounded-md p-4 w-full max-w-lg">
+          <div class="flex justify-between items-center mb-4 gap-6">
+            <div>
+              <span className="text-sm">Price</span>
+              <div class="text-2xl font-semibold">$99.99</div>
             </div>
-              <div class="flex justify-between">
-                <button class="bg-gray-200 text-gray-800 px-5 py-2 rounded-3xl flex items-center">
-                  Customize
-                </button>
-                <button class="bg-gray-200 text-gray-800 px-3 py-2 rounded-3xl flex items-center">
-                  Favorites
-                </button>
-                <button class="bg-gray-200 text-gray-800 px-3 py-2 rounded-3xl flex items-center">
-                  Share
-                </button>
-              </div>
+            <button class="bg-black text-white px-4 py-3 text-sm rounded-3xl w-full flex items-center justify-center">
+              Shop Now
+              <svg
+                class="w-4 h-4 ml-2"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 12h14m0 0l-6-6m6 6l-6 6"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div class="flex justify-between">
+            <button class="bg-gray-200 text-gray-800 px-5 py-2 rounded-3xl flex items-center">
+              Customize
+            </button>
+            <button class="bg-gray-200 text-gray-800 px-3 py-2 rounded-3xl flex items-center">
+              Favorites
+            </button>
+            <button class="bg-gray-200 text-gray-800 px-3 py-2 rounded-3xl flex items-center">
+              Share
+            </button>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
